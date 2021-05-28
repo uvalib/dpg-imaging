697aa2f52883bf5dee76b5bd8654b172353b5946
Dave Goldstein <dpg3k@virginia.edu>
2021-05-28 11:45:57 -0400

    Initial build and packaging assets. Not yet runnable

---
+++ package/scripts/entry.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# run application
+#
+
+# run the server
+#cd bin; ./circulation -solr $CIRCDATA_SOLR_URL -core $CIRCDATA_SOLR_CORE
+
+# return the status
+exit $?
+
+#
+# end of file
+#
