8b7ca8ec689b321a56255bcbab90796266fa64ee
Lou Foster <louffoster@gmail.com>
2021-11-16 16:05:39 -0500

    stub in ui for equipment edit

--- frontend/src/components/project/Equipment.vue
+++ frontend/src/components/project/Equipment.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="panel">
-      <dl>
+      <dl v-if="!editing">
          <dt>Workstation:</dt>
          <dd>
             <span v-if="currProject.workstation.id > 0">{{currProject.workstation.name}}</span>
@@ -34,19 +34,69 @@
             <span v-else class="na">EMPTY</span>
          </dd>
       </dl>
+      <table class="edit" v-else>
+         <tr class="row">
+            <td class="label"><label for="workstation">Workstation:</label></td>
+            <td class="data">
+               <select id="workstation" v-model="workstationID">
+                  <option disabled :value="0">Choose a workstation</option>
+                  <option v-for="ws in workstations" :key="`ws${ws.id}`" :value="ws.id">{{ws.name}}</option>
+               </select>
+            </td>
+         </tr>
+         <tr class="row">
+            <td class="label"><label for="capture">Capture Resolution:</label></td>
+            <td><input id="capture" type="text" v-model="captureResolution"></td>
+         </tr>
+         <tr class="row">
+            <td class="label"><label for="resize">Resized Resolution:</label></td>
+            <td class="data"><input id="resize" type="text" v-model="resizedResolution"></td>
+         </tr>
+         <tr class="row">
+            <td class="label"><label for="res-note">Resolution Note:</label></td>
+            <td class="data"><textarea id="res-note" v-model="resizedResolution"></textarea></td>
+         </tr>
+      </table>
+      <div class="buttons">
+         <DPGButton v-if="!editing" @click="editClicked">Edit</DPGButton>
+         <template v-else>
+            <DPGButton @click="cancelClicked">Cancel</DPGButton>
+            <DPGButton @click="saveClicked">Save</DPGButton>
+         </template>
+      </div>
    </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapState } from "vuex";
 export default {
+   data: function()  {
+      return {
+         editing: false,
+         workstationID: 0,
+         captureRssolution: "",
+         resizedResolution: "",
+         resolutionNote: ""
+      }
+   },
    computed: {
+      ...mapState({
+         workstations: state => state.workstations,
+      }),
       ...mapGetters({
          currProject: "projects/currProject",
       }),
-    },
+   },
    methods: {
-
+      editClicked() {
+         this.editing = true
+      },
+      cancelClicked() {
+         this.editing = false
+      },
+      saveClicked() {
+         this.editing = false
+      }
    },
 };
 </script>
@@ -54,6 +104,37 @@ export default {
 <style scoped lang="scss">
 .panel {
    padding: 10px;
+   .buttons {
+      padding: 0;
+      margin: 0;
+      text-align: right;
+      button {
+         margin-left: 10px;
+      }
+   }
+   .edit {
+      font-size: 0.9em;
+      width: 100%;
+      border-collapse: collapse;
+      margin-bottom: 5px;
+      td {
+          padding: 5px 0px 5px 10px;
+      }
+      td.data {
+         width: 100%;
+      }
+      td.label {
+         font-weight: bold;
+         margin-right: 10px;
+         text-align: right;
+
+         white-space: nowrap;
+      }
+      textarea {
+         width: 100%;
+         box-sizing: border-box;
+      }
+   }
    dl {
       margin: 10px 30px 0 30px;
       display: inline-grid;
