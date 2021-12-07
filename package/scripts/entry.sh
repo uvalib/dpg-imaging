61e69d8f5341234c443b6cec08e5eecdeb3360d6
Lou Foster <louffoster@gmail.com>
2021-12-07 11:44:45 -0500

    added scan dir cfg param

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -5,7 +5,7 @@
 
 # run the server
 umask 0002
-cd bin; ./imagingsvc -url $DPG_SERVICE_URL -images $DPG_IMAGE_PATH -iiif $IIIF_SERVICE_URL -jwtkey $DPG_JWT_KEY -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS
+cd bin; ./imagingsvc -url $DPG_SERVICE_URL -images $DPG_IMAGE_PATH -scan $DPG_SCAN_PATH -iiif $IIIF_SERVICE_URL -jwtkey $DPG_JWT_KEY -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS
 
 # return the status
 exit $?
